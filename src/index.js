62401773a944b4d92df251dcc31b8821539646db
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-11-17 11:52:16 -0400

    Add cinema theme

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+// core components
+import Admin from "layouts/Admin.js";
+import RTL from "layouts/RTL.js";
+import Login from "layouts/Login.js";
+
+import "assets/css/material-dashboard-react.css?v=1.9.0";
+
+const hist = createBrowserHistory();
+const user = true
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {user 
+      ? 
+      <>
+        <Route path="/admin" component={Admin} />
+        <Route path="/rtl" component={RTL} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </>
+      :
+        <Login/>
+      }   
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
