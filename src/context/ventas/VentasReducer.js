c266f545fb356abffab0d908675efc371aef1846
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-11-18 17:46:42 -0400

    Adding VentasContext

---
+++ src/context/ventas/VentasReducer.js
@@ -0,0 +1,19 @@
+export default (state, action) => {
+  switch (action.type) {
+    case "OBTENER_FACTURAS":
+      return{
+        ...state,
+        facturas: state.facturas
+      }
+    case "REMOVER_FACTURA":
+      return {
+        ...state,
+        facturas: state.facturas.filter(
+          (factura) => factura[0] !== action.payload
+          ),
+        facturaSeleccionada: null,
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/context/ventas/VentasState.js
@@ -0,0 +1,47 @@
+import React, { useReducer } from "react";
+import VentasContext from "./VentasContext";
+import VentasReducer from "./VentasReducer";
+
+const currentDate = new Date();
+const VentasState = (props) => {
+  const initialState = {
+    facturas: [
+      [1,"Jose",2,2,"#5",`${currentDate.getDate()}/${currentDate.getMonth()+1}/${currentDate.getFullYear()}`,"Efectivo","$2500"],
+      [2,"Jose",2,2,"#5",`${currentDate.getDate()}/${currentDate.getMonth()+1}/${currentDate.getFullYear()}`,"Efectivo","$2500"],
+      [3,"Jose",2,2,"#5",`${currentDate.getDate()}/${currentDate.getMonth()+1}/${currentDate.getFullYear()}`,"Efectivo","$2500"],
+      [4,"Jose",2,2,"#5",`${currentDate.getDate()}/${currentDate.getMonth()+1}/${currentDate.getFullYear()}`,"Efectivo","$2500"],
+    ],
+    facturaSeleccionada: null,
+  };
+
+  const [state, dispatch] = useReducer(VentasReducer, initialState);
+
+  const obtenerFacturas = ()=>{
+    dispatch({
+      type:"OBTENER_FACTURA"
+    })
+  }
+
+  const removerFactura = (id)=>{
+    dispatch({
+      type: "REMOVER_FACTURA",
+      payload: id
+    })
+  }
+  
+  return (
+    <VentasContext.Provider
+      value={{
+        //State
+        facturas: state.facturas,
+        //Fn
+        removerFactura,
+        obtenerFacturas
+      }}
+    >
+      {props.children}
+    </VentasContext.Provider>
+  );
+};
+
+export default VentasState;
