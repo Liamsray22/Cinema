f0def69cb74f63eb3ada497d7bcd17ff5512c511
Liamsray22 <eliamsolis2201@gmail.com>
2020-11-22 12:54:01 -0400

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,47 +5,67 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
+import 'assets/css/footer.css'
 // core components
 import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";
+
 
 const useStyles = makeStyles(styles);
 
 export default function Footer(props) {
   const classes = useStyles();
   return (
-    <footer className={classes.footer}>
-      <div className={classes.container}>
-        <div className={classes.left}>
-          <List className={classes.list}>
-            <ListItem className={classes.inlineBlock}>
-              <a href="#Inicio" className={classes.block}>
-                Inicio
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a href="#Taquillas" className={classes.block}>
-                Taquillas
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a href="#administracion" className={classes.block}>
-                Administración
-              </a>
-            </ListItem>
-          </List>
+    <div>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+      <script src="https://kit.fontawesome.com/826ecc4006.js" crossorigin="anonymous"></script>
+    
+      <footer class="site-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <h6>Acerca </h6>
+              <p class="text-justify">
+                Software desarrollado por la empresa J&E TECH, capaz de brindar los servicios de facturacion, y
+                administracion de tickets del establecimiento cinematografico E&J CINEMA</p>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <h6>Menu</h6>
+                <ul class="footer-links">
+                  <li><a href="http://localhost:3000/admin/dashboard">Facturacion</a></li>
+                  <li><a href="http://localhost:3000/admin/taquillas">Taquillas</a></li>
+                  <li><a href="http://localhost:3000/admin/ver-facturas">Ver Facturas</a></li>                
+                </ul>
+            </div>
+
+            <div class="col-xs- col-md-3">
+                <h6>Certificaciones NORTIC</h6>
+                <ul>
+                  <li>
+                    <a href="http://optic.gob.do/nortic/index.php/certificaciones/instituciones-certificadas/item/ministerio-de-educacion-minerd" 
+                    hreflang="es" target="_blank" rel="nofollow" title="Medio web certificado bajo la NORTIC A2:2016 con el NIU 16014-01-A216167" >
+                      <img src="http://optic.gob.do/nortic/images/sellos/minerd-sello-a2-2016/01-minerd.png" lang="es" width="83" height="100" alt="Sello de certificación de la A2:2016 con el NIU 16014-01-A216167" /></a>
+                  </li>
+                  <li>
+                    <a href="http://optic.gob.do/nortic/index.php/certificaciones/instituciones-certificadas/item/ministerio-de-educacion-minerd" 
+                    hreflang="es" target="_blank" rel="nofollow" title="Medio web certificado bajo la NORTIC A3:2014 con el NIU 16014-02-A314114" >
+                      <img src="http://optic.gob.do/nortic/images/sellos/minerd-sello-a3-2014/02-minerd.png" lang="es" width="83" height="100" alt="Sello de certificación de la A3:2014 con el NIU 16014-02-A314114" /></a>
+                  </li>
+                </ul>
+              </div>
         </div>
-        <p className={classes.right}>
-          <span>
-            Copyright  &copy; {1900 + new Date().getYear()}{" "} All Rights Reserved by {" "}
-            <a
-              href="#"
-              className={classes.a}
-            >
-              E&J CINEMA
-            </a>
-          </span>
-        </p>
+        <hr/>
       </div>
-    </footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
+         <a href="#">E&J CINEMA</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+</footer>
+    </div>
   );
 }
